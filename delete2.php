da25e8a660aa89c4fe92991fa581f4591db6726c
Murtaza Ahmad <shmurtazaahmad336@gmail.com>
2024-10-03 00:14:20 +0500

    Updated delete2.php and added new logo
    - Modified delete2.php to include feature XYZ (optional detailed description)
    - Added logo.jpg for branding purposes

---
+++ delete2.php
@@ -0,0 +1,15 @@
+<?php
+
+include "dbconnection.php";
+
+$id = $_GET['id'];
+$sql = "DELETE FROM addmission_info WHERE `Std Id` = '$id'";
+$data = mysqli_query($conn,$sql);
+
+if($data){
+    header('location: adm-all-data.php');
+} else{
+    echo "Record Has Not Deleted";
+}
+
+?>
