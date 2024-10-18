8769d37c045dcb541c7ffcdda1779e588b6f828d
Murtaza Ahmad <shmurtazaahmad336@gmail.com>
2024-10-18 23:23:50 +0500

    Updated alumni-data.php and added update3.php
    - Modified alumni-data.php to improve [describe what was changed, e.g., data handling, formatting, or bug fixes].
    - Added update3.php to [describe the purpose of this new file, e.g., handle new feature implementation or update functionality].

---
+++ delete3.php
@@ -0,0 +1,15 @@
+<?php
+
+include "dbconnection.php";
+
+$id = $_GET['id'];
+$sql = "DELETE FROM almuni_form WHERE `Std Id` = '$id'";
+$data = mysqli_query($conn,$sql);
+
+if($data){
+    header('location: almuni-data.php');
+} else{
+    echo "Record Has Not Deleted";
+}
+
+?>
